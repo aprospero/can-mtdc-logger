a6fe0c7b4e2047bbae5f7a75c42831c7c01b3ffb
Aprospero <apro@posteo.de>
2022-11-08 22:32:43 +0100

    adjust scripts to new executable name

--- script/cansorella-mtdc-logger-mqtt.sh
+++ script/cansorella-mtdc-logger-mqtt.sh
@@ -5,7 +5,7 @@ MQTT_USER=<PUT USERNAME HERE>
 MQTT_PASSWORD=<PUT PASSWORD HERE>
 
 IFS=";"
-can-ltdc-logger | while read  timestamp program name value extra; do
+cansorella | while read  timestamp program name value extra; do
 	echo Read: $timestamp $program $name $value $extra
 	if [ "$program" == "DLG_RELAY" ]; then
 		mosquitto_pub -u $MQTT_USER -P $MQTT_PASSWORD -h $MQTT_SERVER -m "$extra"  -t "ltdc/$program/$name/$value" -q 1 -r
